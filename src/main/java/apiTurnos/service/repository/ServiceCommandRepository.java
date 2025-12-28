bca26780f044eae84b7b45c404530cb54f2c499a
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2025-12-27 23:05:12 -0300

    Metodos agregados en los repositories de Command y Query

--- src/main/java/apiTurnos/service/repository/ServiceCommandRepository.java
+++ src/main/java/apiTurnos/service/repository/ServiceCommandRepository.java
@@ -1,4 +1,15 @@
 package apiTurnos.service.repository;
 
-public class ServiceCommandRepository {
+import apiTurnos.service.model.ServiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceCommandRepository extends JpaRepository<ServiceItem, Long> {
+
+    // Metodo para buscar si existe el Servicio con el mismo nombre y activo.
+    boolean existByNameAndActiveTrue(String name);
+
+    // Metodo para buscar si existe el Servicio con el mismo nombre, id y desactivado.
+    boolean existByNameAndIdNotActiveTrue(String name, Long id);
 }
--- src/main/java/apiTurnos/service/repository/ServiceQueryRepository.java
+++ src/main/java/apiTurnos/service/repository/ServiceQueryRepository.java
@@ -2,6 +2,34 @@ package apiTurnos.service.repository;
 
 import apiTurnos.service.model.ServiceItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ServiceQueryRepository extends JpaRepository<ServiceItem, Long> {
+
+    //Consultas especificas para lectura.
+
+    //Listar todos los servicios activos.
+    List<ServiceItem> findAllByActiveTrue();
+
+    // Encontrar un servicio activo por ID.
+    Optional<ServiceItem> findByIdAndActiveTrue(Long id);
+
+    //Consulta especifica a la base de datos.
+    @Query("SELECT s FROM services s WHERE s.active = true AND " +
+    "LOWER(s.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) ")
+
+    // Listar servicios activos en base a una busqueda.
+    List<ServiceItem> searchActiveServices(String searchTerm);
+
+    // Listar servicios con duracion menor a la especificada.
+    List<ServiceItem> findByActiveTrueAndDurationMinutesLessThanEqual(Integer maxDuration);
+
+    // Listar servicios entre el minimo elegido y el maximo elegido.
+    List<ServiceItem> findByActiveTrueAndPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }

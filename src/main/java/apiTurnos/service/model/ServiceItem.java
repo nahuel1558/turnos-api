c0fa8b45400ad97bc5f9a55c99d2171a29770f28
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2025-12-27 23:04:07 -0300

    Agregue atributos al modelo del ServiceItem

--- src/main/java/apiTurnos/service/model/ServiceItem.java
+++ src/main/java/apiTurnos/service/model/ServiceItem.java
@@ -1,8 +1,11 @@
 package apiTurnos.service.model;
 
+import apiTurnos.service.dto.ServiceRequestDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 /**
  * Representa un servicio ofrecido por la peluquería.
  * Ejemplos: Corte, Barba, Corte + Barba.
@@ -23,14 +26,49 @@ public class ServiceItem {
     private Long id;
 
     // Nombre visible del servicio
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String name; // "corte", "Barba", "Corte + Barba".
+
+    // Descripcion del tipo de servicio, no es obligatorio poner una descripcion.
+    @Column(length = 500)
+    private String description;
 
     // Duración del servicio en minutos (30, 45, 60, etc.)
     @Column(nullable = false)
     private Integer durationMinutes; // 30, 45, 60
 
-    // Precio del servicio (opcional en MVP, pero útil comercialmente)
+    // Precio del servicio - "precision" Cantidad de dígitos totales. -/- "scale" Cantidad de decimales.
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Builder.Default
     @Column(nullable = false)
-    private Integer price; // opcional en MVP pero suma
+    private Boolean active = true;
+
+    @Builder.Default
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime updateAt;
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updateAt = LocalDateTime.now();
+    }
+
+    // Metodo para desactivar le service (Soft Delete)
+    public void deactive(){
+        this.active = false;
+        this.updateAt = LocalDateTime.now();
+    }
+
+    // Metodo para actualizar desde el DTO.
+    public void updateFromDTO(ServiceRequestDTO dto){
+        this.name = dto.getName();
+        this.description = dto.getDescription();
+        this.durationMinutes = dto.getDurationMinutes();
+        this.price = dto.getPrice();
+        this.updateAt = LocalDateTime.now();
+    }
 }

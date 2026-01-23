9b7b5f7a9b909a14b7151c286b67ac50b6216d62
Nahuel Gallardo <nahuel.ng240@gmail.com>
2026-01-23 15:52:32 -0300

    refactor(user): ajustar UserMapper

--- src/main/java/apiTurnos/user/mapper/UserMapper.java
+++ src/main/java/apiTurnos/user/mapper/UserMapper.java
@@ -1,4 +1,20 @@
 package apiTurnos.user.mapper;
 
+import apiTurnos.user.dto.response.UserResponse;
+import apiTurnos.user.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserResponse toResponse(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhone(),
+                user.isActive(),
+                user.getRole().stream().map()
+        )
+    }
 }

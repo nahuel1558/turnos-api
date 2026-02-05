aeeceba21eb88fec9102644942f622755d9f55f0
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-02-04 21:22:53 -0300

    feat(client): implementar nuevo metodo existsByUserAccountId
    -Crear metodo nuevo existsByUserAccountId que acepte id tipo string.

--- src/main/java/apiTurnos/client/repository/ClientCommandRepository.java
+++ src/main/java/apiTurnos/client/repository/ClientCommandRepository.java
@@ -4,4 +4,6 @@ import apiTurnos.client.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientCommandRepository extends JpaRepository<Client, Long> {
+
+    boolean existsByUserAccountId(String id);
 }

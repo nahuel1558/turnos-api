ec0f9d98786010a0c14652c5496b84605b010f9c
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-02-04 21:27:01 -0300

    feat(client): implementar nuevos metódos en ClientMapper
    -Crear metódo toResponse mapea un model de Client a ClientResponse
    -Crear metodo toSimpleResponse mapea un modelo de Client a ClientSimpleResponse
    -Crear metodos donde mapeen una lista de model a una lista de response: toResponseList y toSimpleResponseList

--- src/main/java/apiTurnos/client/mapper/ClientMapper.java
+++ src/main/java/apiTurnos/client/mapper/ClientMapper.java
@@ -1,4 +1,44 @@
 package apiTurnos.client.mapper;
 
+import apiTurnos.client.model.Client;
+import apiTurnos.client.presentation.dto.response.ClientResponse;
+import apiTurnos.client.presentation.dto.response.ClientSimpleResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ClientMapper {
+
+    public ClientResponse toResponse(Client client){
+        return ClientResponse.builder()
+                .id(client.getId())
+                .userId(client.getUserAccount().getId())
+                .email(client.getEmail())
+                .fullName(client.getFullName())
+                .firstName(client.getUserAccount().getFirstName())
+                .lastName(client.getUserAccount().getLastName())
+                .phone(client.getUserAccount().getPhone())
+                .clientSince(client.getClientSince())
+                .userStatus(client.getUserAccount().getStatus())
+                .build();
+    }
+
+    public ClientSimpleResponse toSimpleResponse(Client client){
+        return ClientSimpleResponse.builder()
+                .id(client.getId())
+                .userId(client.getUserAccount().getId())
+                .fullName(client.getFullName())
+                .email(client.getEmail())
+                .phone(client.getUserAccount().getPhone())
+                .build();
+    }
+
+
+    public List<ClientResponse> toResponseList(List<Client> clientList){
+        return clientList.stream().map(this::toResponse).collect(Collectors.toList());
+    }
+
+    public List<ClientSimpleResponse> toSimpleResponseList(List<Client> clientList){
+        return clientList.stream().map(this::toSimpleResponse).collect(Collectors.toList());
+    }
 }

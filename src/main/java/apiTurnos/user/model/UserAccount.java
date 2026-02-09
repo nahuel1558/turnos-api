67df37a849b37c452d43a42ffe89b371bff67012
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-02-09 18:26:16 -0300

    refactor(userAccount): Modificar Role role en UserAccount - Cambiar Role role a Set<SystemRole> roles creando relacion de many to many. - Agrear metodos nuevos addRole, remoceRole y hasRole

--- src/main/java/apiTurnos/user/model/UserAccount.java
+++ src/main/java/apiTurnos/user/model/UserAccount.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Entidad de Dominio/Persistencia del usuario.
@@ -41,8 +43,6 @@ public class UserAccount {
     @Column(nullable = false, length = 80)
     private String lastName;
 
-    @Column(nullable = false, length = 80)
-    private String phone;
 
     /**
      * Guardar SIEMPRE hash, nunca plain text.
@@ -50,9 +50,17 @@ public class UserAccount {
     @Column(nullable = false, length = 255)
     private String passwordHash;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
-    private Role role;
+    /**
+     *  Relacion con los roles
+     */
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_account_roles",
+            joinColumns = @JoinColumn(name = "user_account_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<SystemRole> roles = new HashSet<>();
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
@@ -69,6 +77,24 @@ public class UserAccount {
     // -------------------------
     // Comportamiento de dominio
     // -------------------------
+
+    public void addRole(SystemRole role) {
+        if (role != null && !this.roles.contains(role)) {
+            this.roles.add(role);
+            markUpdated(null);
+        }
+    }
+
+    public void removeRole(SystemRole role) {
+        if (role != null) {
+            this.roles.remove(role);
+            markUpdated(null);
+        }
+    }
+
+    public boolean hasRole(String roleName) {
+        return roles.stream().anyMatch(role -> role.getName().equals(roleName));
+    }
 
     public void markUpdated(LocalDateTime now) {
         this.updatedAt = (now == null) ? LocalDateTime.now() : now;

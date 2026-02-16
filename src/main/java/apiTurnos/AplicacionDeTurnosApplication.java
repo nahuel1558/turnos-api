119843350b67dc6e969d5a77b7994fb4e3c9806b
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-02-16 16:27:37 -0300

    refactor!: Eliminar linea de zona horaria hardcodeado

--- src/main/java/apiTurnos/AplicacionDeTurnosApplication.java
+++ src/main/java/apiTurnos/AplicacionDeTurnosApplication.java
@@ -3,12 +3,9 @@ package apiTurnos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class AplicacionDeTurnosApplication {
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(AplicacionDeTurnosApplication.class, args);
     }
 }

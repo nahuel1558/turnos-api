3aec1055210b6f190ae23c25f6cd93b1ade1a062
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-02-16 16:41:01 -0300

    refactor(service): Modificar nombres en ServiceCommandRepository
    - Agregar la letra "s" despues de "exist" a "exists"

--- src/main/java/apiTurnos/service/infrastructure/repository/ServiceCommandRepository.java
+++ src/main/java/apiTurnos/service/infrastructure/repository/ServiceCommandRepository.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 public interface ServiceCommandRepository extends JpaRepository<ServiceItem, Long> {
 
     // Metodo para buscar si existe el Servicio con el mismo nombre y active.
-    boolean existByNameAndActiveTrue(String name);
+    boolean existsByNameAndActiveTrue(String name);
 
     // Metodo para buscar si existe el Servicio con el mismo nombre, id y desactivado.
-    boolean existByNameAndIdNotActiveTrue(String name, Long id);
+    boolean existsByNameAndActiveTrueAndIdNot(String name, Long id);
 }
